4fc161e1e8eb7f46afe9514c6bbdf147efe127c2
mac lawson <macattackairman@gmail.com>
2024-06-13 13:18:59 -0400

    removed :os_mon extra

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Dynamind.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :os_mon]
+      extra_applications: [:logger]
     ]
   end
 
