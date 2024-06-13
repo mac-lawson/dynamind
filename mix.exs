156fca5b160177e8568e58a592ab38c94652f693
mac lawson <macattackairman@gmail.com>
2024-06-13 13:13:27 -0400

    removed unneeded deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dynamind.MixProject do
     [
       app: :dynamind,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: escript()
@@ -26,15 +26,10 @@ defmodule Dynamind.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:bumblebee, "~> 0.5.3"},
       {:plug_cowboy, "~> 2.6"},
       {:jason, "~> 1.4"},
-      # Bumblebee and friends
-      {:nx, "~> 0.7.0"},
       # Exqlite is a simple SQLite3 driver for Elixir
       {:exqlite, "~> 0.17"},
-      # Exla is a wrapper around the Google TensorFlow C API
-      {:exla, "~> 0.7.1"},
       {:luerl, "~> 1.2"},
       {:httpoison, "~> 1.8"},
       {:mox, "~> 1.0", only: :test}
